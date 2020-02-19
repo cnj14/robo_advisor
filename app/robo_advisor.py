4a6c778a9c13530758325dc43c06cb4ff75946e8
cnj14 <59627768+cnj14@users.noreply.github.com>
2020-02-19 00:35:54 -0500

    Initial output setup

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -5,9 +5,12 @@ import json
 import os
 from dotenv import load_dotenv
 import csv
+from datetime import datetime
 
 load_dotenv()
 
+now = datetime.now()
+current_time = now.strftime("%I:%M%p on %Y-%m-%d")
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "oops")
 count = 0
@@ -24,12 +27,19 @@ while count < n:
         else:
             parsed_response = json.loads(response.text)
             tsd = parsed_response["Time Series (Daily)"]
+            mdata = parsed_response["Meta Data"]
             csv_file_path = f"data/{SYMBOL}.csv"
             with open(csv_file_path,"w") as csv_file:
                 writer = csv.DictWriter(csv_file, fieldnames=["timestamp", "open","high","low","close","volume"])
                 writer.writeheader()
                 for date,values in tsd.items():
                     writer.writerow({"timestamp": date, "open": values["1. open"], "high": values["2. high"], "low": values["3. low"], "close": values["4. close"], "volume": values["5. volume"]})
+            print("------------------------------------")
+            print(f"Stock selected: {SYMBOL}")
+            print(f"Program run at {current_time}")
+            latest = mdata["3. Last Refreshed"]
+            print(f"Latest data from {latest}")
+            print("------------------------------------")
             count+=1
     else:
         print("Please try a an alphabetic ticker with 5 or fewer letters.")
