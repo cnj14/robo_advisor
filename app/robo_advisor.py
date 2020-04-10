31d669a5029f1a86460e5437e148c43d62b4ec69
cnj14 <59627768+cnj14@users.noreply.github.com>
2020-04-10 19:05:33 -0400

    Script update

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -6,15 +6,58 @@ import os
 from dotenv import load_dotenv
 import csv
 from datetime import datetime
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
 load_dotenv()
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "oops")
 
 now = datetime.now()
 current_time = now.strftime("%I:%M%p on %Y-%m-%d")
 
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "oops")
+def lines():
+    print("------------------------------------")
+
+def to_usd(price):
+    return "${0:,.2f}".format(price)
+
+def get_response(symbol):
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    return parsed_response
+
+def transform_response(parsed_response):
+    tsd = parsed_response["Time Series (Daily)"]
+    rows = []
+    for date, prices in tsd.items():
+        row = {
+            "timestamp": date,
+            "open": float(prices["1. open"]),
+            "high": float(prices["2. high"]),
+            "low": float(prices["3. low"]),
+            "close": float(prices["4. close"]),
+            "volume": int(prices["5. volume"])
+        }
+        rows.append(row)
+    return rows
+
+def csv_writer(rows, filepath):
+    with open(filepath, 'w') as csv_file:
+        writer = csv_DictWriter(csv_file, fieldnames = ['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+    return True
+
+def get_decision(symbol,last,high,low):
+    diff = high - low
+    midpoint = low + diff/2
+    symbol = symbol.upper()
+    if last<midpoint:
+        return f'BUY! {symbol} IS PRICED CLOSER TO ITS RECENT LOW THAN HIGH.'
+    else:
+        return f'SELL! {symbol} IS PRICED CLOSER TO ITS RECENT HIGH THAN LOW.'
+
 count = 0
 n = (input("How many stocks would you like to consider? "))
 if n.isnumeric() == True:
@@ -29,108 +72,32 @@ if n.isnumeric() == True:
         else:
             print("Please try an alphabetic ticker with 5 or fewer letters.")
     for SYMBOL in symbols:
-        request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={SYMBOL}&apikey={API_KEY}"
-        response = requests.get(request_url)
-        if "Error Message" in response.text:
-            print("Error received; please try again! Enter a valid ticker such as 'AAPL' or 'VZ'.")
-        else:
-            print("------------------------------------")
-            print("ACCESSING DATA...")
-            parsed_response = json.loads(response.text)
-            tsd = parsed_response["Time Series (Daily)"]
-            mdata = parsed_response["Meta Data"]
-            csv_file_path = f"data/{SYMBOL}.csv"
-            with open(csv_file_path,"w") as csv_file:
-                writer = csv.DictWriter(csv_file, fieldnames=["timestamp", "open","high","low","close","volume"])
-                writer.writeheader()
-                for date,values in tsd.items():
-                    writer.writerow({"timestamp": date, "open": values["1. open"], "high": values["2. high"], "low": values["3. low"], "close": values["4. close"], "volume": values["5. volume"]})
-            print("------------------------------------")
-            print(f"Stock selected: {SYMBOL}")
-            print(f"Program run at {current_time}")
-            latest = mdata["3. Last Refreshed"]
-            print(f"Latest data from {latest}")
-            print("------------------------------------")
-            with open(csv_file_path, "r") as csv_file: 
-                reader = csv.DictReader(csv_file) 
-                all_closes = []
-                all_highs = []
-                all_lows = []
-                dates = []
-                for row in reader:
-                    close = float(row["close"])
-                    all_closes.append(close)
-                    high = float(row["high"])
-                    all_highs.append(high)
-                    low = float(row["low"])
-                    all_lows.append(low)
-                    date = row["timestamp"]
-                    dates.append(date)
-                last_close = (all_closes[0])
-                last_close_1 = '${:,.2f}'.format(last_close)
-                recent_max = max(all_highs)
-                recent_max_1 = '${:,.2f}'.format(recent_max)
-                recent_min = min(all_lows)
-                recent_min_1 = '${:,.2f}'.format(recent_min)
-            print(f"Most recent closing price: {last_close_1}")
-            print(f"Recent high price: {recent_max_1}")
-            print(f"Recent low price: {recent_min_1}")
-            for p in all_closes:
-                p = "${0:.2f}".format(p)
-            all_closes.reverse()
-            dates.reverse()
-            fig1, ax = plt.subplots()
-            ax.plot(dates, all_closes)
-            formatter = ticker.FormatStrFormatter('$%1.2f')
-            ax.yaxis.set_major_formatter(formatter)
-            plt.title(f"{SYMBOL} Stock (Last 100 Days)")
-            fig1.autofmt_xdate()
-            # https://stackoverflow.com/questions/6682784/reducing-number-of-plot-ticks/13418954#13418954
-            every_nth = 9
-            for n, label in enumerate(ax.xaxis.get_ticklabels()):
-                if n % every_nth != 0:
-                    label.set_visible(False)
-            plt.show()
-            print("------------------------------------")
-            print("RUNNING PROPRIETARY INVESTMENT ALGORITHM...")
-            daily_returns = []
-            gains = []
-            losses = []
-            all_closes.reverse()
-            for i in range(99):
-                r = all_closes[i]-all_closes[i+1]
-                r = r/all_closes[i+1]
-                daily_returns.append(r)
-            for r in daily_returns:
-                if r>0:
-                    gains.append(r)
-                else:
-                    losses.append(r)
-            price_range = (recent_max-recent_min)
-            midpoint = (recent_min + recent_max)/2
-            percentile = 100*((last_close-recent_min)/price_range)
-            percentile = '{:,.2f}'.format(percentile)
-            print(f"In the last 100 days, {SYMBOL} has had {len(gains)} daily gains \nand {len(losses)} daily losses from close to close.")
-            print('')
-            print(f"{SYMBOL} is currently priced at {last_close_1} which is \n in the {percentile} percentile of the 100-day range.")
-            print('')
-            if len(gains)>55 and midpoint>last_close:
-                print(f"{SYMBOL} has had daily gains more than 55% of the time and is \n still closer to its recent low than high.")
-                print(f"We recommend {SYMBOL} as a BUY opportunity.")
-            elif len(gains)>55 and midpoint<last_close:
-                print(f"{SYMBOL} has had daily gains more than 55% of the time but is \n closer to its recent high than low.")
-                print(f"We recommend HOLDING for the moment on {SYMBOL}.")
-            elif len(gains)<55 and midpoint>last_close:
-                print(f"{SYMBOL} is closer to its recent low than high, but \n has had daily gains less than 55% of the time.")
-                print(f"We recommend HOLDING for the moment on {SYMBOL} but watching closely.")
-            elif len(gains)<55 and midpoint<last_close:
-                print(f"{SYMBOL} is closer to its recent high than low and \n has had daily gains less than 55% of the time.")
-                print(f"We recommend {SYMBOL} as a SELL opportunity.")
-            print("------------------------------------")
+        parsed_response = get_response(SYMBOL)
+        latest = parsed_response["Meta Data"]["3. Last Refreshed"]
+        
+        rows = transform_response(parsed_response)
+        last_close = rows[0]['close']
+        high_prices = [row["high"] for row in rows]
+        low_prices = [row["low"] for row in rows]
+        recent_high = max(high_prices)
+        recent_low = min(low_prices)
+
+        lines()
+        print(f"SYMBOL: {SYMBOL.upper()}")
+        lines()
+        print(f'REQUEST FULFILLED AT {current_time}')
+        print(f'ACCESSING DATA FROM {latest}')
+        lines()
+        print(f"LATEST CLOSE: {to_usd(last_close)}")
+        print(f"RECENT HIGH:  {to_usd(recent_high)}")
+        print(f"RECENT LOW:   {to_usd(recent_low)}")
+        lines()
+        print('RUNNING PROPRIETARY INVESTMENT ALGORITHM...')
+        print(f'RECOMMENDATION: {get_decision(SYMBOL,last_close,recent_high,recent_low)}')
+        lines()
     print("Thank you for using the Robo Advisor! Happy investing :)")
 else:
     print("Invalid number of stocks entered. Please run code again!")
     exit()
-
 
 
